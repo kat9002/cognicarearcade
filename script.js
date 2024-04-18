05717e10f1932a559599be17231deb579c11bb69
kat9002 <167364116+kat9002@users.noreply.github.com>
2024-04-19 05:20:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
+//import Firebase libraries needed to connect to database
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getDatabase, ref, child, get} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
+import { getStorage, ref as sRef, getDownloadURL} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-storage.js";
+
+//configuration for Firebase
+  const firebaseConfig = {
+    apiKey: "AIzaSyCIIIWf-HcWXVOqqD82mcxy-3Xx3AtgDcc",
+    authDomain: "facerecognitiontestdb.firebaseapp.com",
+    databaseURL: "https://facerecognitiontestdb-default-rtdb.firebaseio.com",
+    projectId: "facerecognitiontestdb",
+    storageBucket: "facerecognitiontestdb.appspot.com",
+    messagingSenderId: "733228864305",
+    appId: "1:733228864305:web:adba281c0d53a7709bd610"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
+  //Retrieving Firebase Database to retrieve the labels for the uploaded users
+  const getDB = getDatabase();
+
+  //Accessing Firebase Storage to retrieve the images of users
+  const storage = getStorage();
+
+
 const video = document.getElementById("video");
 
+    const labels = [];
+    const dTokens = [];
+
+    const dbRef = ref(getDB);
+    get(child(dbRef, 'username')).then((snapshot)=>{
+        var data = snapshot.val();
+        for(let i in data){
+
+            labels.push(data[i].replaceAll('"', '')); 
+
+            get(child(dbRef, data[i].replaceAll('"', ''))).then((snapshot)=>{
+            var data2 = snapshot.val();
+            for(let i in data2){
+    
+                dTokens.push(data2[i].replaceAll('"', ''));           
+            }
+            console.log(dTokens)
+        })          
+        }
+    })
+    
+
 Promise.all([
-  faceapi.nets.ssdMobilenetv1.loadFromUri("weights"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("weights"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("weights"),
+  faceapi.nets.ssdMobilenetv1.loadFromUri("./weights"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("./weights"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("./weights"),
 ]).then(startWebcam);
 
 function startWebcam() {
@@ -20,20 +68,31 @@ function startWebcam() {
     });
 }
 
+
+
 function getLabeledFaceDescriptions() {
-  const labels = ["Amor Larque", "Danica Godornes", "Janine Godornes", "Kat Godornes", "Ella Canque", "Dex Salva"];
+
+    
+
   return Promise.all(
     labels.map(async (label) => {
       const descriptions = [];
+        var a = label;
+        var token = dTokens[labels.indexOf(label)];
 
-        const img = await faceapi.fetchImage(`labels/${label}/1.jpg`);
+        var file = `https://firebasestorage.googleapis.com/v0/b/facerecognitiontestdb.appspot.com/o/${label.replaceAll(' ', '%20')}%2F1.jpg?alt=media&token=`+token
+        
+        console.log(token)
+        console.log(file)
+        const img = await faceapi.fetchImage(file);
         const detections = await faceapi
           .detectSingleFace(img)
           .withFaceLandmarks()
           .withFaceDescriptor();
         descriptions.push(detections.descriptor);
-      
+        
       return new faceapi.LabeledFaceDescriptors(label, descriptions);
+      
     })
   );
 }
